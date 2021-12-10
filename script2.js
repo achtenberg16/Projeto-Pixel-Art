5783cd7c071a61f4e8b349531d3956d558890a80
Alessandro Achtenberg <achtenberg16@gmail.com>
2021-12-10 20:35:24 -0300

    refatora eestrutura do cógido

--- script2.js
+++ script2.js
@@ -1,22 +1,43 @@
-let generateColor = () => `rgb(${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)})`
+const pixelBoard = document.getElementById('pixel-board');
+const sizeQuare = document.getElementById('generate-board');
+const clear = document.getElementById('clear-board');
+const newcolor = document.getElementById('newColor');
 
-function listColor() {
-  const elementos = document.getElementsByClassName('color');
-  for (let index = 1; index < elementos.length; index += 1) {
-    const novaCor = generateColor();
-    elementos[index].style.backgroundColor = generateColor();
-  }
+const creators = {
+  //esta função cria uma lista com 3 cores para completar a paleta juntamente a cor preta
+  listColor: () => {
+    const elementos = document.getElementsByClassName('color');
+    for (let index = 1; index < elementos.length; index += 1) {
+      elementos[index].style.backgroundColor = creators.generateColor();
+    }
+  },
+  //esta função cria todos os pixels dentro das linhas, a quantidade de elementos criados é definido pela quantidade
+  //de linhas criadas
+  createElement: () => {
+    const pixelBoards = document.querySelectorAll('.line');
+    for (let index = 0; index < pixelBoards.length; index += 1) {
+      for (let index2 = 0; index2 < pixelBoards.length; index2 += 1) {
+        const cria = document.createElement('div');
+        cria.classList.add('pixel');
+        pixelBoards[index].appendChild(cria);
+      }
+    }
+    
+  },
+  //está função gera um parametro rgb aleatório ex rgb(155,212, 24)
+  generateColor: () => `rgb(${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)})`
+  
 }
 
-function iniciar() {
-  const pai = document.getElementById('pixel-board');
-  //pai.innerText = '';
+
+function init() {
+  pixelBoard.innerText = '';
   for (let index = 0; index < 5; index += 1) {
     const cria = document.createElement('div');
     cria.classList.add('line');
-    pai.appendChild(cria);
+    pixelBoard.appendChild(cria);
   }
-  criaElementos();
+  creators.createElement();
 }
 
 function corAtual(event) {
@@ -33,8 +54,8 @@ function inicio() {
   for (let index = 0; index < paleta.length; index += 1) {
     paleta[index].addEventListener('click', corAtual);
   }
-  iniciar();
-  listColor();
+  init();
+  creators.listColor();
 }
 
 window.onload = inicio;
@@ -45,61 +66,40 @@ function Pintar(event) {
   event.target.style.backgroundColor = colorSelected;
 }
 
-function adicionaEventos() {
-  const elementos = document.getElementsByClassName('pixel');
-  for (let index = 0; index < elementos.length; index += 1) {
-    elementos[index].addEventListener('click', Pintar);
-  }
-}
-
-function criaElementos() {
-  const pais = document.querySelectorAll('.line');
-  for (let index = 0; index < pais.length; index += 1) {
-    for (let index2 = 0; index2 < pais.length; index2 += 1) {
-      const cria = document.createElement('div');
-      cria.classList.add('pixel');
-      pais[index].appendChild(cria);
-    }
-  }
-  adicionaEventos();
-}
 function CriaLinhas() {
-  const pai = document.getElementById('pixel-board');
-  pai.innerText = '';
+  const pixelBoard = document.getElementById('pixel-board');
+  pixelBoard.innerText = '';
   const quantidade = document.getElementById('board-size');
-  const valor2 = quantidade.value;
-  for (let index = 0; index < valor2; index += 1) {
+  const sizeQuare2 = quantidade.value;
+  for (let index = 0; index < sizeQuare2; index += 1) {
     const cria = document.createElement('div');
     cria.classList.add('line');
-    pai.appendChild(cria);
+    pixelBoard.appendChild(cria);
   }
-  criaElementos();
+  creators.createElement();
 }
 function sizeBoard() {
   const quantidades = document.getElementById('board-size');
-  const valor3 = quantidades.value;
-  if (!valor3) { return alert('Board inválido!'); }
-  if (valor3 < 5) {
+  const sizeQuare3 = quantidades.value;
+  if (!sizeQuare3) { return alert('Board inválido!'); }
+  if (sizeQuare3 < 5) {
     quantidades.value = 5;
     return CriaLinhas();
   }
-  if (valor3 > 50) {
+  if (sizeQuare3 > 50) {
     quantidades.value = 50;
     return CriaLinhas();
   }
   return CriaLinhas();
 }
-const valor = document.getElementById('generate-board');
-valor.addEventListener('click', sizeBoard);
 
 function ClearColor() {
-  const elementosLimpar = document.getElementsByClassName('pixel');
-  for (let index = 0; index < elementosLimpar.length; index += 1) {
-    elementosLimpar[index].style.backgroundColor = 'white';
+  const clerElement = document.getElementsByClassName('pixel');
+  for (let index = 0; index < clerElement.length; index += 1) {
+    clerElement[index].style.backgroundColor = 'white';
   }
 }
-const clear = document.getElementById('clear-board');
 clear.addEventListener('click', ClearColor);
-const newColor = document.getElementById('newColor');
-
-newColor.addEventListener('click', listColor);
+sizeQuare.addEventListener('click', sizeBoard);
+newcolor.addEventListener('click', creators.listColor);
+pixelBoard.addEventListener('click', Pintar);
