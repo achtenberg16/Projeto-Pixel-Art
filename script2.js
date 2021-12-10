be42baa65ab894c39433b0ff7c0fb983165b8c28
Alessandro Achtenberg <achtenberg16@gmail.com>
2021-12-10 16:15:32 -0300

    refatora eestrutura do cógido

--- script2.js
+++ script2.js
@@ -1,13 +1,16 @@
-function novasCores() {
+let generateColor = () => `rgb(${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)})`
+
+function listColor() {
   const elementos = document.getElementsByClassName('color');
   for (let index = 1; index < elementos.length; index += 1) {
-    const novaCor = `rgb(${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)},${parseInt(Math.random() * 264)})`;
-    elementos[index].style.backgroundColor = novaCor;
+    const novaCor = generateColor();
+    elementos[index].style.backgroundColor = generateColor();
   }
 }
+
 function iniciar() {
   const pai = document.getElementById('pixel-board');
-  pai.innerText = '';
+  //pai.innerText = '';
   for (let index = 0; index < 5; index += 1) {
     const cria = document.createElement('div');
     cria.classList.add('line');
@@ -15,6 +18,7 @@ function iniciar() {
   }
   criaElementos();
 }
+
 function corAtual(event) {
   const cores = document.querySelectorAll('.color');
   for (let index = 0; index < cores.length; index += 1) {
@@ -23,21 +27,24 @@ function corAtual(event) {
   const evento = event.target;
   evento.classList.add('selected');
 }
+
 function inicio() {
   const paleta = document.querySelectorAll('.color');
   for (let index = 0; index < paleta.length; index += 1) {
     paleta[index].addEventListener('click', corAtual);
   }
   iniciar();
-  novasCores();
+  listColor();
 }
+
 window.onload = inicio;
+
 function Pintar(event) {
-  const elementoAtual = document.getElementsByClassName('selected')[0];
-  const corAtual2 = window.getComputedStyle(elementoAtual).backgroundColor;
-  const evento = event.target;
-  evento.style.backgroundColor = corAtual2;
+  const elementSelected = document.getElementsByClassName('selected')[0];
+  const colorSelected = window.getComputedStyle(elementSelected).backgroundColor;
+  event.target.style.backgroundColor = colorSelected;
 }
+
 function adicionaEventos() {
   const elementos = document.getElementsByClassName('pixel');
   for (let index = 0; index < elementos.length; index += 1) {
@@ -68,7 +75,7 @@ function CriaLinhas() {
   }
   criaElementos();
 }
-function verifica() {
+function sizeBoard() {
   const quantidades = document.getElementById('board-size');
   const valor3 = quantidades.value;
   if (!valor3) { return alert('Board inválido!'); }
@@ -83,16 +90,16 @@ function verifica() {
   return CriaLinhas();
 }
 const valor = document.getElementById('generate-board');
-valor.addEventListener('click', verifica);
+valor.addEventListener('click', sizeBoard);
 
-function limpaFundo() {
+function ClearColor() {
   const elementosLimpar = document.getElementsByClassName('pixel');
   for (let index = 0; index < elementosLimpar.length; index += 1) {
     elementosLimpar[index].style.backgroundColor = 'white';
   }
 }
 const clear = document.getElementById('clear-board');
-clear.addEventListener('click', limpaFundo);
+clear.addEventListener('click', ClearColor);
 const newColor = document.getElementById('newColor');
 
-newColor.addEventListener('click', novasCores);
+newColor.addEventListener('click', listColor);
